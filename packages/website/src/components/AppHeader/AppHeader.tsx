28ab76a1b7fca44f586d94990e01261693cecba5
Hikari Hayashi <rev.hikari@gmail.com>
2022-02-13 12:26:28 +0800

    Add logo in header

--- packages/website/src/components/AppHeader/AppHeader.tsx
+++ packages/website/src/components/AppHeader/AppHeader.tsx
@@ -1,3 +1,4 @@
+import { LogoIcon } from "@neetly/icons";
 import { Link } from "@neetly/router";
 
 import { APP_NAME } from "../../constants";
@@ -7,7 +8,10 @@ const AppHeader = () => {
   return (
     <header className={styles.header}>
       <Link className={styles.logoLink} to="/">
-        <h1 className={styles.logo}>{APP_NAME}</h1>
+        <h1 className={styles.logo}>
+          <LogoIcon className={styles.logoIcon} />
+          <span className={styles.logoText}>{APP_NAME}</span>
+        </h1>
       </Link>
     </header>
   );
