b870b4ff9f581b3f409f993c1b294516024440aa
Hikari Hayashi <rev.hikari@gmail.com>
2022-04-14 14:54:09 +0800

    Remove css file

--- scripts/palette_codegen.py
+++ scripts/palette_codegen.py
@@ -33,21 +33,6 @@ with open("./packages/palette/src/generated/index.ts", "w") as file:
     file.write("export { palette };\n")
 
 
-with open("./packages/palette/src/generated/index.css", "w") as file:
-    file.write("/* This file is auto-generated. */\n\n")
-    file.write(":root {\n")
-    for index, (
-        group_name,
-        items,
-    ) in enumerate(itertools.groupby(palette, get_group_name)):
-        if index != 0:
-            file.write("\n")
-        file.write(f"  /* {group_name} */\n")
-        for item in items:
-            file.write(f"  --palette--{get_id(item['name'])}: {item['color']};\n")
-    file.write("}\n")
-
-
 with open("./packages/palette/src/generated/index.scss", "w") as file:
     file.write("// This file is auto-generated.\n\n")
     for index, (
