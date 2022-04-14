30581adcec75911ea5746edb10710c02ac3db11b
Hikari Hayashi <rev.hikari@gmail.com>
2022-04-14 18:11:03 +0800

    Update

--- packages/website/src/components/AppHeader/AppHeader.tsx
+++ packages/website/src/components/AppHeader/AppHeader.tsx
@@ -3,14 +3,14 @@ import { VisuallyHidden } from "@neetly/ui";
 
 import { APP_NAME } from "../../constants";
 import styles from "./AppHeader.module.scss";
-import LogoIcon from "./assets/LogoIcon.svg";
+import LogoTextIcon from "./assets/LogoTextIcon.svg";
 
 const AppHeader = () => {
   return (
     <header className={styles.header}>
       <Link className={styles.logoLink} to="/">
         <h1 className={styles.logo}>
-          <LogoIcon className={styles.logoIcon} />
+          <LogoTextIcon className={styles.logoTextIcon} />
           <VisuallyHidden>{APP_NAME}</VisuallyHidden>
         </h1>
       </Link>
