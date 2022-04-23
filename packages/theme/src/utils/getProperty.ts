089afaa3b26980cf8caef7e5bf3ce07c498ca3aa
Hikari Hayashi <rev.hikari@gmail.com>
2022-04-23 16:54:08 +0800

    Clean up

--- packages/theme/src/utils/getProperty.ts
+++ packages/theme/src/utils/getProperty.ts
@@ -1,11 +1,7 @@
 import { getPropertyName } from "./getPropertyName";
 
-const getProperty = (name: string, value: string | null = null) => {
-  if (value !== null) {
-    return `var(${getPropertyName(name)}, ${value})`;
-  } else {
-    return `var(${getPropertyName(name)})`;
-  }
+const getProperty = (name: string) => {
+  return `var(${getPropertyName(name)})`;
 };
 
 export { getProperty };
