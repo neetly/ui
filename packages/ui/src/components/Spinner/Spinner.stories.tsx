567668b680ec9ed08e262d62e5c80ab2642be56c
Hikari Hayashi <rev.hikari@gmail.com>
2022-04-23 16:23:55 +0800

    Add storybook

---
+++ packages/ui/src/components/Spinner/Spinner.stories.tsx
@@ -0,0 +1,7 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react/types-7-0";
+
+import { Spinner } from "./Spinner";
+
+export default { component: Spinner } as ComponentMeta<typeof Spinner>;
+
+export const Default: ComponentStory<typeof Spinner> = {};
