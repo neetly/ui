aa759e0c1cb514110f60b434356ffce541705cb9
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-04 02:21:27 +0800

    Add search icon

---
+++ packages/icons/src/generated/SearchIcon.tsx
@@ -0,0 +1,9 @@
+import type { SVGProps } from "react";
+
+const SvgSearchIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" aria-hidden={true} {...props}>
+    <path d="M10 3a7 7 0 1 0 0 14 7 7 0 0 0 0-14Zm-5 7a5 5 0 1 1 10 0 5 5 0 0 1-10 0Zm14.293 10.707-4.387-4.387a8.05 8.05 0 0 0 1.414-1.414l4.387 4.387a1 1 0 0 1-1.414 1.414Z" />
+  </svg>
+);
+
+export default SvgSearchIcon;
--- packages/icons/src/generated/index.ts
+++ packages/icons/src/generated/index.ts
@@ -2,3 +2,4 @@ export { default as ChevronLeftIcon } from "./ChevronLeftIcon";
 export { default as ChevronRightIcon } from "./ChevronRightIcon";
 export { default as CloseIcon } from "./CloseIcon";
 export { default as LogoIcon } from "./LogoIcon";
+export { default as SearchIcon } from "./SearchIcon";
