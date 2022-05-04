1e8fc3b6f4dea871f6b268f6531caf9618efaf43
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-04 15:55:15 +0800

    Use preview-head.html

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -1,7 +1,1 @@
 import "./preview.scss";
-
-const link = document.createElement("link");
-link.rel = "stylesheet";
-link.href =
-  "https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,400..700;1,400..700";
-document.head.appendChild(link);
