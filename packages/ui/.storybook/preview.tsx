d73bf68f8dae8b2e246e3bb5076dce4b494fcb7c
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-04 15:09:09 +0800

    Use Source Sans 3

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -1,1 +1,7 @@
 import "./preview.scss";
+
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href =
+  "https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,400..700;1,400..700";
+document.head.appendChild(link);
