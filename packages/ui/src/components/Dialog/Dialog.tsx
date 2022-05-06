b8b78add5da08b3167e356e942eb7efbf0d87488
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-07 00:08:35 +0800

    Update

--- packages/ui/src/components/Dialog/Dialog.tsx
+++ packages/ui/src/components/Dialog/Dialog.tsx
@@ -1,7 +1,19 @@
 import classNames from "classnames";
-import type { ComponentPropsWithoutRef, ReactNode } from "react";
+import type {
+  ComponentPropsWithoutRef,
+  ReactNode,
+  SyntheticEvent,
+} from "react";
+import { useLayoutEffect, useRef } from "react";
 
 import styles from "./Dialog.module.scss";
+
+declare module "react" {
+  interface DOMAttributes<T> {
+    onClose?: React.ReactEventHandler<T> | undefined;
+    onCancel?: React.ReactEventHandler<T> | undefined;
+  }
+}
 
 type DialogOwnProps = {
   className?: string;
@@ -13,9 +25,38 @@ type DialogOwnProps = {
 type DialogProps = DialogOwnProps &
   Omit<ComponentPropsWithoutRef<"dialog">, keyof DialogOwnProps>;
 
-const Dialog = ({ className, children, ...props }: DialogProps) => {
+const Dialog = ({
+  className,
+  open = true,
+  onClose,
+  children,
+  ...props
+}: DialogProps) => {
+  const ref = useRef<HTMLDialogElement>(null);
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      if (open && !ref.current.open) {
+        ref.current.showModal();
+      }
+      if (!open && ref.current.open) {
+        ref.current.close();
+      }
+    }
+  }, [open]);
+
+  const onCancel = (event: SyntheticEvent) => {
+    event.preventDefault();
+    onClose?.();
+  };
+
   return (
-    <dialog className={classNames(styles.dialog, className)} {...props}>
+    <dialog
+      ref={ref}
+      className={classNames(styles.dialog, className)}
+      onCancel={onCancel}
+      {...props}
+    >
       {children}
     </dialog>
   );
