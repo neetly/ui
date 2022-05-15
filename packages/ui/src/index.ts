6ae83e505f14ee10a35c733e3c055c8874f9ad2b
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-15 09:49:53 +0800

    Clean up

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./components/Button";
 export * from "./components/Checkbox";
+export * from "./components/Dialog";
 export * from "./components/Polymorphic";
 export * from "./components/Radio";
 export * from "./components/Spinner";
