a7c5c6f8e71bd9353b56ff848a4dfc7abea5cfd3
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-24 11:55:47 +0800

    Rename some icons

--- packages/icons/src/generated/MediaStopIcon.tsx
+++ packages/icons/src/generated/MediaStopIcon.tsx
@@ -1,9 +1,9 @@
 import type { SVGAttributes } from "react";
 
-const SvgStopIcon = (props: SVGAttributes<SVGSVGElement>) => (
+const SvgMediaStopIcon = (props: SVGAttributes<SVGSVGElement>) => (
   <svg viewBox="0 0 24 24" aria-hidden={true} {...props}>
     <path d="M7 6h10a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1Z" />
   </svg>
 );
 
-export default SvgStopIcon;
+export default SvgMediaStopIcon;
