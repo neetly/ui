1d5bd66bba44e99db2e614a428f7d3c0b4faddc1
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-24 14:48:42 +0800

    Add more icons

---
+++ packages/icons/src/generated/FavoriteOutlineIcon.tsx
@@ -0,0 +1,9 @@
+import type { SVGAttributes } from "react";
+
+const SvgFavoriteOutlineIcon = (props: SVGAttributes<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" aria-hidden={true} {...props}>
+    <path d="M12.464 5.964a5 5 0 1 1 7.071 7.072l-6.828 6.828a1 1 0 0 1-1.414 0l-6.829-6.829a5 5 0 1 1 7.072-7.07l.464.464.464-.465Zm5.657 1.415a3 3 0 0 0-4.242 0L12.707 8.55a1 1 0 0 1-1.414 0L10.12 7.38a3 3 0 1 0-4.24 4.24L12 17.743l6.121-6.122a3 3 0 0 0 0-4.242Z" />
+  </svg>
+);
+
+export default SvgFavoriteOutlineIcon;
---
+++ packages/icons/src/generated/MoreIcon.tsx
@@ -0,0 +1,9 @@
+import type { SVGAttributes } from "react";
+
+const SvgMoreIcon = (props: SVGAttributes<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" aria-hidden={true} {...props}>
+    <path d="M13.5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 11a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm-1.5-4a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" />
+  </svg>
+);
+
+export default SvgMoreIcon;
