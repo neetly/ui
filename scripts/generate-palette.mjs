4783768687a9d2a84b272042140228f56646df42
Hikari Hayashi <rev.hikari@gmail.com>
2022-07-10 16:06:10 +0800

    Update palette generator

---
+++ scripts/generate-palette.mjs
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { clampChroma, formatHex } from "culori";
+import fs from "fs";
+
+const schema = JSON.parse(fs.readFileSync("./data/palette/schema.json"));
+
+const palette = [];
+for (const role of schema.roles) {
+  for (const tone of schema.tones) {
+    const color = {
+      mode: "oklch",
+      l: (tone.tone / 100) * 0.85 + 0.15,
+      c: (role.chroma / 100) * 0.3,
+      h: role.hue,
+    };
+
+    palette.push({
+      id: `${role.id}/${tone.id}`,
+      color: formatHex(clampChroma(color, "oklch")),
+    });
+  }
+}
+
+fs.writeFileSync("./data/palette/palette.json", JSON.stringify(palette));
