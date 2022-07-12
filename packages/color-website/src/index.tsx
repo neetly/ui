e255bf6139c6cd8a8ab358cd021d1924719c45ad
Hikari Hayashi <rev.hikari@gmail.com>
2022-07-13 00:25:26 +0800

    Enable strict mode

--- packages/color-website/src/index.tsx
+++ packages/color-website/src/index.tsx
@@ -1,10 +1,15 @@
 import "./index.scss";
 
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 
 const container = document.querySelector("#root");
 if (container) {
-  createRoot(container).render(<App />);
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
 }
