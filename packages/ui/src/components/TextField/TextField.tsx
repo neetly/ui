13c95b6c9693f6492eca354e1904c66031966295
Hikari Hayashi <rev.hikari@gmail.com>
2022-07-14 22:57:48 +0800

    Revert "Update text field label html structure"
    This reverts commit 438d06402e4b84c2d14365fc1c52abfae75c1051.

--- packages/ui/src/components/TextField/TextField.tsx
+++ packages/ui/src/components/TextField/TextField.tsx
@@ -2,13 +2,11 @@ import type { IconProps } from "@neetly/icons";
 import classNames from "classnames";
 import type { ComponentType, ElementType, ReactNode } from "react";
 
-import { useId } from "../../hooks/useId";
 import type { PolymorphicProps } from "../Polymorphic";
 import { createPolymorphicComponent } from "../Polymorphic";
 import styles from "./TextField.module.scss";
 
 type TextFieldOwnProps = {
-  id?: string;
   className?: string;
   label?: ReactNode;
   iconBefore?: ComponentType<IconProps>;
@@ -23,7 +21,6 @@ const TextField = createPolymorphicComponent(
   "TextField",
   <Element extends ElementType = "input">({
     as,
-    id,
     className,
     label,
     iconBefore: IconBefore,
@@ -32,21 +29,15 @@ const TextField = createPolymorphicComponent(
     ...props
   }: TextFieldProps<Element>) => {
     const Component = as ?? "input";
-    id = useId(id);
 
     return (
-      <span
+      <label
         className={classNames(styles.container, className)}
         data-disabled={disabled ? "" : null}
       >
-        {label && (
-          <label className={styles.label} htmlFor={id}>
-            {label}
-          </label>
-        )}
+        <span className={styles.label}>{label}</span>
         <span className={styles.content}>
           <Component
-            id={id}
             className={styles.textField}
             data-icon-before={IconBefore ? "" : null}
             data-icon-after={IconAfter ? "" : null}
@@ -56,7 +47,7 @@ const TextField = createPolymorphicComponent(
           {IconBefore && <IconBefore className={styles.iconBefore} />}
           {IconAfter && <IconAfter className={styles.iconAfter} />}
         </span>
-      </span>
+      </label>
     );
   },
 );
--- packages/ui/src/hooks/useId.ts
+++
@@ -1,8 +0,0 @@
-import { useId as useReactId } from "react";
-
-const useId = (id?: string) => {
-  const defaultId = useReactId();
-  return id ?? defaultId;
-};
-
-export { useId };
